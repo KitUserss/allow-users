334827cc88cb4d6c8d3a0bdfd52369be0103a722
KitUserss <naotemsuporte@gmail.com>
2026-01-11 18:03:55 -0300

    Update and rename README.md to brookhaven.lua

---
+++ brookhaven.lua
@@ -0,0 +1,21 @@
+return {
+    "Breno010L", -- Bazukita
+    "RUANRODRIGO121121", -- Ryuzak
+    "Gabriel263633j", -- Passarinho
+    "rafamemeaa", -- rafael PERM
+    "Patodev_lenda", -- Karyston
+    "PowerLegendSpark2018", -- Dev Spawn
+    "Master_gustax", -- kid beng...
+    "Gustavo25022", -- kid beng...
+    "Wolfgang_227", -- Soares
+    "NeoX_neon", -- Farofa
+    "olliver2045judoca", -- frost
+    "antiscam_XD", -- hacer 11/01 até 14/01
+    "3000turbo", -- 300turbo 11/01 até 18/01
+    "tizianoasdfghjkl", -- thiago 11/01 até 14/01
+    "DicasdeIngles", -- lua inf/inf até inf/inf
+    "murilo_12303", -- kayrus 08/01 até 12/01
+    "jonn_mascarado", -- itadori 08/01 até 13/01
+    "LUIGI_CRIA9987", -- shawn 08/01 até 12/01
+    "julinha014802" -- inf/inf até inf/inf
+}
