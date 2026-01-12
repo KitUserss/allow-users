56bcebd2f0843cc12e0a59b827457615af808e29
KitUserss <naotemsuporte@gmail.com>
2026-01-12 14:48:55 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -14,7 +14,7 @@ return {
     "antiscam_XD", -- hacer 11/01 14/01
     "3000turbo", -- 300turbo 11/01 18/01
     "tizianoasdfghjkl", -- thiago 11/01 14/01
-    "DicasdeIngles", -- lua inf/inf inf/inf
+    "DicasdeIngles", -- lua 11/01 12/02
     "murilo_12303", -- kayrus 08/01 12/01
     "jonn_mascarado", -- itadori 08/01  13/01
     "LUIGI_CRIA9987", -- shawn 08/01  12/01
