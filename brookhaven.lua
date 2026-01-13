abc3d3fde58fc760e9e6c925c5e24bf7fed1f6cf
KitUserss <naotemsuporte@gmail.com>
2026-01-13 15:31:13 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -11,6 +11,7 @@ return {
     "Danguel11", -- calanguin divulgador
     "olliver2045judoca", -- frost
     "eduardo_billgate", -- gustavo 11/01 14/01
+    "sansu2449", -- mv Divulgador
     "INC0MUUUU90", -- absolute 13/01  20/01
     "antiscam_XD", -- hacer 11/01 14/01
     "3000turbo", -- 300turbo 11/01 18/01
