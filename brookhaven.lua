a81ec9715bae9978bbd7caaecf2fd8a91bf2d31e
KitUserss <naotemsuporte@gmail.com>
2026-01-16 14:09:24 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -12,12 +12,11 @@ return {
     "Danguel11", -- calanguin divulgador
     "olliver2045judoca", -- frost
     "sansu2449", -- mv Divulgador
-    "joaow8wjww", -- joao 16/01 23/01
     "INC0MUUUU90", -- absolute 13/01  20/01
     "3000turbo", -- 300turbo 11/01 18/01
     "DicasdeIngles", -- lua 11/01 12/02
     "Neto934y22", -- 13/01 16/01
-    "julinha014802", -- inf/inf inf/inf 
+    "joaow8wjww", -- inf/inf inf/inf 
     "THANKS0117", -- nicolas 12/01 15/01
     "BIELZIN_0I" -- 12/01  15/01
 }
