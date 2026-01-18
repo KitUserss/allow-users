0631641220a7050f076d23cbce6c03c316441240
KitUserss <naotemsuporte@gmail.com>
2026-01-18 17:05:29 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -3,20 +3,13 @@ return {
     "RUANRODRIGO121121", -- Ryuzak
     "Gabriel263633j", -- Passarinho
     "JustWX99s", -- Dabruxis
-    "Patodev_lenda", -- Karyston
     "PowerLegendSpark2018", -- Dev Spawn
-    "Master_gustax", -- kid beng...
-    "Gustavo25022", -- kid beng...
     "Wolfgang_227", -- Soares
     "NeoX_neon", -- Farofa
     "Danguel11", -- calanguin divulgador
     "olliver2045judoca", -- frost
     "sansu2449", -- mv Divulgador
     "INC0MUUUU90", -- absolute 13/01  20/01
-    "3000turbo", -- 300turbo 11/01 18/01
     "DicasdeIngles", -- lua 11/01 12/02
-    "Neto934y22", -- 13/01 16/01
     "joaow8wjww", -- 15/01 15/03
-    "THANKS0117", -- nicolas 12/01 15/01
-    "BIELZIN_0I" -- 12/01  15/01
 }
