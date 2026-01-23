b83862300c2a71a29640703f7672d98095a5751a
KitUserss <naotemsuporte@gmail.com>
2026-01-23 01:36:10 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -9,6 +9,7 @@ return {
     "Danguel11", -- calanguin divulgador
     "olliver2045judoca", -- frost
     "sansu2449", -- mv Divulgador
+    "Helena_om8", -- 23/01  26/01
     "N3bula_Xr0131", -- N3bula 22/01  25/01
     "DicasdeIngles", -- lua 11/01 12/02
     "joaow8wjww" -- 15/01 15/03
