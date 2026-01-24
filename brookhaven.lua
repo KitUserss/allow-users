842bfc085a20e4cec5ef71246830473a4842f457
KitUserss <naotemsuporte@gmail.com>
2026-01-24 10:41:43 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -12,6 +12,11 @@ return {
     "Helena_om8", -- 23/01  26/01
     "N3bula_Xr0131", -- N3bula 22/01  25/01
     "DicasdeIngles", -- lua 11/01 12/02
-    "joaow8wjww" -- 15/01 15/03 
+    "eduardo_billgate", -- gustavo 24/01  26/01
+    "murilo_12303", -- kayrus 24/01  26/01
+    "asUoWFQaKS", -- batatadoida 24/01  26/01
+    "INC0MUUUU90", -- absolute 24/01  26/01
+    "nolyhaha", -- lol 24/01  26/01
+    "joaow8wjww", -- joao 15/01 15/03 
     "Guiheheke" -- 23/01 26/01
 }
