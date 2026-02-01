85535bc10dffa31ea5fa98d559326e9f3f7f1f90
KitUserss <naotemsuporte@gmail.com>
2026-02-01 02:04:43 -0300

    Atualizar o brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -9,15 +9,7 @@ return {
     "Danguel11", -- calanguin divulgador
     "olliver2045judoca", -- frost
     "sansu2449", -- mv Divulgador
-    "Helena_om8", -- 23/01  26/01
-    "N3bula_Xr0131", -- N3bula 22/01  25/01
     "DicasdeIngles", -- lua 11/01 12/02
-    "eduardo_billgate", -- gustavo 24/01  26/01
-    "murilo_12303", -- kayrus 24/01  26/01
-    "asUoWFQaKS", -- batatadoida 24/01  26/01
-    "INC0MUUUU90", -- absolute 24/01  26/01
-    "nolyhaha", -- lol 24/01  26/01
     "joaow8wjww", -- joao 15/01 15/03 
-    "Guiheheke", -- 24/01 27/01
     "vivi4ne16" -- 01/02 /01/03
 }
