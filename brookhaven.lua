a4e65f1e9c725f954c375f961f5284e08a4f054b
KitUserss <naotemsuporte@gmail.com>
2026-02-04 02:56:48 -0300

    Atualizar o brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -11,5 +11,6 @@ return {
     "sansu2449", -- mv Divulgador
     "DicasdeIngles", -- lua 11/01 12/02
     "joaow8wjww", -- joao 15/01 15/03 
-    "vivi4ne16" -- 01/02 /01/03
+    "vivi4ne16", -- 01/02 /01/03
+    "Tennyson_10ben6" -- teste
 }
