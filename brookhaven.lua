ece384f1cffd022fc5cdc31f7e5d24430f5553f1
KitUserss <naotemsuporte@gmail.com>
2026-02-16 19:46:38 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -16,6 +16,7 @@ return {
     "joaow8wjww", -- joao 15/01 18/03
     "pumaji32", -- 16/02 19/02
     "asafegaga", -- 16/02 19/02
+    "soubrabo0101", -- 16/02 19/02
     "luan122010", -- 16/02 19/02
     "ColorTristanm5071", -- 10/02 13/03
     "isagisei1" -- filho do ryu
