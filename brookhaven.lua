55fc2db88a485aa1487b746f9ebaf2310d3f5a6f
KitUserss <naotemsuporte@gmail.com>
2026-02-17 19:09:49 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -1,29 +1,29 @@
 return {
-    "Breno010L", -- Bazukita
-    "lushmart", -- bazuka
-    "RUANRODRIGO121121", -- Ryuzak
-    "Gabriel263633j", -- Passarinho
-    "JustWX99s", -- Dabruxis
-    "PowerLegendSpark2018", -- Dev Spawn
-    "Wolfgang_227", -- Soares
-    "Meka_js", -- Farofa
-    "olliver2045judoca", -- frost
-    "taylinho4509", -- Gabriel Divulgador
-    "ImQiakEzoHy", -- King Divulgador
-    "rip_dragon1236016", -- alan divulgador
-    "sansu2449", -- mv Divulgador
-    "sansu78906", -- mv Divulgador
-    "Luckzvxs", -- Luckzv 15/02 28/02
-    "itz_davix86", -- indio 15/02 25/02
-    "joaow8wjww", -- joao 15/01 18/03
-    "pumaji32", -- 16/02 19/02
-    "Pedrolucca_093", -- 16/02 19/02
-    "asafegaga", -- 16/02 19/02
-    "Mizaelfm2", -- 16/02 19/02
-    "soubrabo0101", -- 16/02 19/02
-    "luan122010", -- 16/02 19/02
-    "ColorTristanm5071", -- 10/02 13/03
-    "isagisei1", -- filho do ryu
-    "joaoking667", -- 17/02 20/02
-    "manuela4h4" -- manu
+    "Breno010L",
+    "lushmart",
+    "RUANRODRIGO121121",
+    "Gabriel263633j",
+    "JustWX99s",
+    "PowerLegendSpark2018",
+    "Wolfgang_227",
+    "Meka_js",
+    "olliver2045judoca",
+    "taylinho4509",
+    "ImQiakEzoHy",
+    "rip_dragon1236016",
+    "sansu2449",
+    "sansu78906",
+    "Luckzvxs",
+    "itz_davix86",
+    "joaow8wjww",
+    "pumaji32",
+    "Pedrolucca_093",
+    "asafegaga",
+    "Mizaelfm2",
+    "soubrabo0101",
+    "luan122010",
+    "ColorTristanm5071",
+    "isagisei1",
+    "joaoking667",
+    "manuela4h4"
 }
