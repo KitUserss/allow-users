2959fcf990021fe1f5eeb4ca52578d095ee3b9aa
KitUserss <naotemsuporte@gmail.com>
2026-02-25 15:50:30 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -14,13 +14,12 @@ return {
     "sansu2449", -- mv Divulgador
     "sansu78906", -- mv Divulgador
     "Luckzvxs", -- Luckzv 15/02 28/02
-    "itz_davix86", -- indio 15/02 25/02
-    "joaoking667", -- JoaoExe 21/02 24/02
     "joaow8wjww", -- joao 15/01 18/03
     "GabsszinZX", -- gbszx 21/02 28/02
+    "Fernandes12395", -- King 25/02 28/02
     "ColorTristanm5071", -- 10/02 13/03
     "Davhi755", -- TikTok 23/02 26/02
     "antiscam_XD", -- Joao Vitor 22/02 29/02
-    "isagisei1" -- filho do ryu
+    "isagisei1", -- filho do ryu
     "crlh_nathyy" -- nathy
 }
