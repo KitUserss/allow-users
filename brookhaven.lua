3b74b6862e95b6adaccbe35b07cedf6053bf6b5d
KitUserss <naotemsuporte@gmail.com>
2026-02-28 18:14:09 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -8,21 +8,13 @@ return {
     "Wolfgang_227", -- Soares
     "Meka_js", -- Farofa
     "olliver2045judoca", -- frost
-    "taylinho4509", -- Gabriel Divulgador
-    "ImQiakEzoHy", -- King Divulgador
-    "rip_dragon1236016", -- alan divulgador
+    "ziovxoo", -- slow divulgador
     "sansu2449", -- mv Divulgador
     "sansu78906", -- mv Divulgador
-    "Luckzvxs", -- Luckzv 15/02 28/02
     "joaow8wjww", -- joao 15/01 18/03
-    "GabsszinZX", -- gbszx 21/02 28/02
-    "Fernandes12395", -- King 25/02 28/02
     "ColorTristanm5071", -- 10/02 13/03
-    "Davhi755", -- TikTok 23/02 26/02
     "antiscam_XD", -- Joao Vitor 22/02 29/02
-    "Vexmyrk412", -- 25/02 28/02
     "isagisei1", -- filho do ryu
     "K1LL_py", -- kill 26/02  04/03
-    "crlh_nathyy", -- nathy
     "Hey_zPUDIM" -- 26/02 29/02
 }
