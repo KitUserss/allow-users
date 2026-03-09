39cc64aa0abe5aea4cd80f78574d43d517b2c7bc
KitUserss <naotemsuporte@gmail.com>
2026-03-08 22:10:02 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -1,21 +1,12 @@
 return {
     "Breno010L", -- Bazukita
     "StaffordMakayla783", -- bazuka
-    "RUANRODRIGO121121", -- Ryuzak
     "Gabriel263633j", -- Passarinho
     "JustWX99s", -- Dabruxis
     "PowerLegendSpark2018", -- Dev Spawn
-    "Meka_js", -- Farofa
     "olliver2045judoca", -- frost
     "gtavaitoma01", -- kakashi divulgador
-    "Luckzvxs", -- Luckzvxs divulgador
-    "Olivio922", -- k1ng divulgador
-    "Alvin99H", -- Alvin divulgador
-    "dszin0_0", -- divulgador
-    "Markespvp_S", -- thex divulgador
-    "n7criak", -- snow divulgador
-    "ziovxoo", -- slow divulgador
-    "aliliolas", -- mostii divulgador
+    "ziovxoo", -- Xuung Divulgador
     "sansu2449", -- mv Divulgador
     "sansu78906", -- mv Divulgador
     "andrielebibi", -- driih 03/03  03/07
@@ -26,13 +17,11 @@ return {
     "ColorTristanm5071", -- 10/02 13/03
     "joaoking667", -- joao 01/03  18/03
     "Antiscam_xd", -- Joao vitor 06/03 11/03
-    "isagisei1", -- filho do ryu
     "K1LL_py", -- kill 04/03  04/07
     "YAGOZXNF", -- king 02/03  08/03
     "silvaa_lenda", -- 07/03 27/03
     "daorabo8", -- Ovosoldado 02/03  08/03
     "tremdamidia55", -- 07/03  10/03
-    "baconjabiraca", -- wzz 
     "SUCOdeDIGIMON", -- 02/03 08/03
     "isaac_344725", -- 07/03  08/03
     "Gabsszinzx", -- Gabs 07/03  14/03
