0bad9d8df95387b82d1c04bc6260a1131354abaf
KitUserss <naotemsuporte@gmail.com>
2026-03-09 23:27:01 -0300

    Update dates for user entries in brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -11,7 +11,7 @@ return {
     "sansu78906", -- mv Divulgador
     "andrielebibi", -- driih 03/03  03/07
     "joaow8wjww", -- joao 15/01 18/03
-    "DanieR7Play", -- DnZinN 05/03  05/04
+    "DanieR7Play", -- DnZinN 05/03  12/04
     "163637227", -- theus 07/03  14/03
     "Patodev_lenda", -- Multilaser 07/03  14/03
     "ColorTristanm5071", -- 10/02 13/03
@@ -19,8 +19,9 @@ return {
     "Antiscam_xd", -- Joao vitor 06/03 11/03
     "K1LL_py", -- kill 04/03  04/07
     "YAGOZXNF", -- king 02/03  08/03
-    "silvaa_lenda", -- 07/03 27/03
+    "silvaa_lenda", -- 07/03 03/04
     "daorabo8", -- Ovosoldado 02/03  08/03
+    "TUBER9307514", -- 09/03  16/03
     "tremdamidia55", -- 07/03  10/03
     "SUCOdeDIGIMON", -- 02/03 08/03
     "isaac_344725", -- 07/03  08/03
