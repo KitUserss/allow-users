2f5a434df54b6192d3b79b3b577d79ee8d6446d9
KitUserss <naotemsuporte@gmail.com>
2026-03-12 13:12:39 -0300

    Modify user dates and add new user entry
    Updated the date for 'SUCOdeDIGIMON' and added 'NSC_TALISSON14218'.

--- brookhaven.lua
+++ brookhaven.lua
@@ -27,8 +27,9 @@ return {
     "Ternkqx_150", -- 09/03 16/03
     "eduardo_billgate", -- 09/03  16/03
     "tremdamidia55", -- 07/03  10/03
-    "SUCOdeDIGIMON", -- 02/03 08/03
+    "SUCOdeDIGIMON", -- 02/03 15/03
     "isaac_344725", -- 07/03  15/03
+    "NSC_TALISSON14218", -- 12/02  19/02
     "Gabsszinzx", -- Gabs 07/03  14/03
     "AGHOSTTHEDOOWCOOL" -- 05/03 12/03
 }
