17ab3d0a5b9ba72abc562e4fc1e308ab437b3830
KitUserss <naotemsuporte@gmail.com>
2026-03-18 18:31:17 -0300

    Update bazukita

--- brookhaven.lua
+++ brookhaven.lua
@@ -1,41 +1,42 @@
 return {
-    "Breno010L", -- Bazukita
-    "StaffordMakayla783", -- bazuka
-    "RUANRODRIGO121121", -- Ryuzak
-    "Gabriel263633j", -- Passarinho
-    "JustWX99s", -- Dabruxis
-    "PowerLegendSpark2018", -- Dev Spawn
-    "olliver2045judoca", -- frost
-    "ziovxoo", -- Xuung Divulgador
-    "sansu2449", -- mv Divulgador
-    "sansu78906", -- mv Divulgador
-    "andrielebibi", -- driih 03/03  03/07
-    "joaow8wjww", -- joao 15/01 18/03
-    "DanieR7Play", -- DnZinN 05/03  12/04
-    "163637227", -- theus 13/03 25/03
-    "Patodev_lenda", -- Multilaser 07/03  14/03
-    "ColorTristanm5071", -- 10/02 13/03
-    "joaoking667", -- joao 01/03  18/03
-    "vteteus", -- Pietro 15/03  22/03
-    "Antiscam_xd", -- Joao vitor 06/03 11/03
-    "K1LL_py", -- kill 04/03  04/07
-    "YAGOZXNF", -- king 02/03  08/03
-    "silvaa_lenda", -- 07/03 03/04
-    "daorabo8", -- Ovosoldado 02/03  08/03
-    "TUBER9307514", -- 09/03  23/03
-    "Luan122010", -- 09/03  23/03
-    "Ternkqx_150", -- 09/03 16/03
-    "eduardo_billgate", -- 09/03  16/03
-    "tremdamidia55", -- 07/03  10/03
-    "SUCOdeDIGIMON", -- 02/03 22/03
-    "isaac_344725", -- 07/03  15/03
-    "NSC_TALISSON14218", -- 12/02  19/03
-    "ellernate_ofc04", -- 12/02  19/03
-    "Fernandes12395", -- 12/02 19/03
-    "o9j77", -- 14/03 19/03
-    "mizaelfm2", -- 14/03  19/03
-    "itz_davix86", -- 12/03  19/03
-    "Gabsszinzx", -- Gabs 07/03  21/03
-    "AGHOSTTHEDOOWCOOL" -- 05/03 12/03,
-    "aineymar", -- 18/03 19/03
+    "Breno010L", --Bazukita
+    "StaffordMakayla783", --bazuka
+    "RUANRODRIGO121121", --Ryuzak
+    "Gabriel263633j", --Passarinho
+    "JustWX99s", --Dabruxis
+    "PowerLegendSpark2018", --Dev Spawn
+    "olliver2045judoca", --frost
+    "ziovxoo", --Xuung Divulgador
+    "sansu2449", --mv Divulgador
+    "sansu78906", --mv Divulgador
+    "andrielebibi", --driih 03/03  03/07
+    "joaow8wjww", --joao 15/01 18/03
+    "DanieR7Play", --DnZinN 05/03  12/04
+    "163637227", --theus 13/03 25/03
+    "Patodev_lenda", --Multilaser 07/03  14/03
+    "ColorTristanm5071", --10/02 13/03
+    "joaoking667", --joao 01/03  18/03
+    "vteteus", --Pietro 15/03  22/03
+    "Antiscam_xd", --Joao vitor 06/03 11/03
+    "K1LL_py", --kill 04/03  04/07
+    "YAGOZXNF", --king 02/03  08/03
+    "silvaa_lenda", --07/03 03/04
+    "daorabo8", --Ovosoldado 02/03  08/03
+    "TUBER9307514", --09/03  23/03
+    "Luan122010", --09/03  23/03
+    "Ternkqx_150", --09/03 16/03
+    "eduardo_billgate", --09/03  16/03
+    "tremdamidia55", --07/03  10/03
+    "SUCOdeDIGIMON", --02/03 22/03
+    "isaac_344725", --07/03  15/03
+    "NSC_TALISSON14218", --12/02  19/03
+    "ellernate_ofc04", --12/02  19/03
+    "Fernandes12395", --12/02 19/03
+    "o9j77", --14/03 19/03
+    "mizaelfm2", --14/03  19/03
+    "itz_davix86", --12/03  19/03
+    "Gabsszinzx", --Gabs 07/03  21/03
+    "AGHOSTTHEDOOWCOOL", --05/03 12/03,
+    "aineymar", --18/03 19/03
+    "bazukita" --18/03 21/03
 }
