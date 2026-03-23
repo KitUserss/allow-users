e7283cf1f00d28fc90e1f1ee570ac7a67579f389
KitUserss <naotemsuporte@gmail.com>
2026-03-22 23:00:30 -0300

    Remove ColorTristanm5071

--- brookhaven.lua
+++ brookhaven.lua
@@ -14,7 +14,6 @@ return {
     "DanieR7Play", -- DnZinN 05/03  12/04
     "163637227", -- theus 13/03 25/03
     "Patodev_lenda", -- Multilaser 07/03  14/03
-    "ColorTristanm5071", -- 10/02 13/03
     "K1LL_py", -- kill 04/03  04/07
     "silvaa_lenda", -- 07/03 03/04
     "TUBER9307514", -- 09/03  23/03
