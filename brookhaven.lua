f0119c970b08308f470db4cc4842d19dfd42891c
KitUserss <naotemsuporte@gmail.com>
2026-03-23 23:28:11 -0300

    Auto remove expirados: aaaa

--- brookhaven.lua
+++ brookhaven.lua
@@ -16,6 +16,5 @@ return {
     "silvaa_lenda", -- 07/03 03/04
     "o9j77", -- 23/03 25/03
     "Hetien", -- 23/03 22/04
-    "magosupremo132", -- 23/03 26/03
-    "aaaa" -- arabiano 23/03/26 21/03/26
+    "magosupremo132" -- 23/03 26/03
 }
