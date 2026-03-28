5f41d54f08dfef6235902f26389807fadfd5f351
KitUserss <naotemsuporte@gmail.com>
2026-03-28 16:21:34 -0300

    Remove l_rrz01l_rrz01

--- brookhaven.lua
+++ brookhaven.lua
@@ -14,6 +14,5 @@ return {
     "K1LL_py", -- kill 04/03 04/07
     "silvaa_lenda", -- 07/03 03/04
     "Hetien", -- 23/03 22/04
-    "Mateusx9992", -- João Vitor 28/03/26 02/04/26
-    "l_rrz01l_rrz01" -- 𝐀𝐥𝐩𝐡𝐚' 28/03/26 02/04/26
+    "Mateusx9992" -- João Vitor 28/03/26 02/04/26
 }
