06559b47ea434c66e068527dbb57604669ea60b5
KitUserss <naotemsuporte@gmail.com>
2026-04-05 08:36:04 -0300

    Remove l_rrz01

--- brookhaven.lua
+++ brookhaven.lua
@@ -18,6 +18,5 @@ return {
     "Milena_Martins77", -- ꧁ .𝕮𝖗𝖚𝖊𝖑𝖑𝖆 .꧂ Staff 29/03/26 13/08/53
     "xVenomDev", -- 𝚅𝚎𝚗𝚘𝚖 02/04/26 18/08/53
     "Hetien", -- ༺ Jolli ༻ 02/04/26 18/08/53
-    "l_rrz01", -- ألفا 02/04/26 27/12/28
     "ddrrfdrrrrrrr" -- absoluto zero 🫪 03/04/26 20/01/68
 }
