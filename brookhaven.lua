2fc67e1f4d6f99952944eb420b4352b9c0c731a9
KitUserss <naotemsuporte@gmail.com>
2026-04-26 00:40:21 -0300

    Update eduardo_billgate

--- brookhaven.lua
+++ brookhaven.lua
@@ -14,7 +14,7 @@ return {
     "andrielebibi", -- driih 03/03 03/07
     "K1LL_py", -- kill 04/03 04/07
     "daviiie08", -- 07/03 03/04
-    "eduardo_billgate", -- Duoc 01/04/26 17/08/53
+    "eduardo_billgate", -- 𝔇𝔲𝔪 #F the best 26/04/26 10/09/53
     "Milena_Martins77", -- 𝕮𝖗𝖚𝖊𝖑𝖑𝖆 𝕱𝖆𝖗𝖗𝖆𝖕𝖔 25/04/26 21/03/64
     "xVenomDev", -- 𝚅𝚎𝚗𝚘𝚖 02/04/26 18/08/53
     "Hetien", -- ! 3ʇuɐʍsʞɔɐןᙠ 23/04/26 08/09/53
