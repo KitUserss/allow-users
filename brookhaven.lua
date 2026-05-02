cc7cea6f4d8619b25a24386528e4ef7c014d9c49
KitUserss <naotemsuporte@gmail.com>
2026-05-02 19:00:19 -0300

    Remove duplicate user entries in brookhaven.lua
    Removed duplicate entries for 'sadzin131' and 'vteteus' from the list.

--- brookhaven.lua
+++ brookhaven.lua
@@ -8,8 +8,10 @@ return {
     "olliver2045judoca", -- frost Mod Ult
     "ziovxoo", -- Xuung Divulgador
     "sansu2449", -- mv Divulgador
+    "sadzin131", -- pietro divulgador
     "sansu78906", -- mv Divulgador
     "isaac_344725", -- Gagagay divulgador
+    "vteteus", -- [Divulgador] Pietro 07/04/26 01/01/29
     "higorFARM_FM", -- Higzada "possível divulgador"
     "andrielebibi", -- driih 03/03 03/07
     "adm_Zago98" -- diego permanente
@@ -19,8 +21,6 @@ return {
     "Milena_Martins77", -- 𝕮𝖗𝖚𝖊𝖑𝖑𝖆 𝕱𝖆𝖗𝖗𝖆𝖕𝖔 25/04/26 21/03/64
     "xVenomDev", -- 𝚅𝚎𝚗𝚘𝚖 02/04/26 18/08/53
     "PrimalC00kie201892", -- unknown999 05/04/26 06/05/26
-    "sadzin131", -- pietro divulgador
-    "vteteus", -- [Divulgador] Pietro 07/04/26 01/01/29
     "marcodoido28", -- _NUNES_ 28/04/26 24/03/64
     "ddrrfdrrrrrrr", -- absoluto zero lindo 24/04/26 28/06/45
     "freezx108", -- 𝕱𝖗𝖊𝖊𝖟⁷ 25/04/26 09/09/53
