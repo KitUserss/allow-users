779fcd516bcd5cc72adf801d4895ad49be296b15
KitUserss <naotemsuporte@gmail.com>
2026-05-03 00:45:44 -0300

    Auto remove expirados: rockyfan133kk

--- brookhaven.lua
+++ brookhaven.lua
@@ -25,7 +25,6 @@ return {
     "ddrrfdrrrrrrr", -- absoluto zero lindo 24/04/26 28/06/45
     "freezx108", -- 𝕱𝖗𝖊𝖊𝖟⁷ 25/04/26 09/09/53
     "simeumsm3", -- ★~𝓛𝓪𝓻𝓲 ~★ 26/04/26 22/03/64
-    "rockyfan133kk", -- Rocky 26/04/26 03/05/26
     "LUIGI_CRIA9987", -- Sky 26/04/26 11/09/53
     "Soares_rlk65", -- SOARES 27/04/26 08/03/59
     "KuonNL", -- 𝔇𝔲𝔪 ANGEL 27/04/26 05/03/59
