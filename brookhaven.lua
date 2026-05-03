b777bf6c348bb9fe9c33992b76b8764c3575ee32
KitUserss <naotemsuporte@gmail.com>
2026-05-02 21:28:03 -0300

    Fix formatting of adm_Zago98 entry in brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -15,7 +15,7 @@ return {
     "vteteus", -- [Divulgador] Pietro 07/04/26 01/01/29
     "higorFARM_FM", -- Higzada "possível divulgador"
     "andrielebibi", -- driih 03/03 03/07
-    "adm_Zago98" -- diego permanente
+    "adm_Zago98", -- diego permanente
     "K1LL_py", -- kill 04/03 04/07
     "eduardo_billgate", -- 𝔇𝔲𝔪 #F the best 26/04/26 10/09/53
     "Milena_Martins77", -- 𝕮𝖗𝖚𝖊𝖑𝖑𝖆 𝕱𝖆𝖗𝖗𝖆𝖕𝖔 25/04/26 21/03/64
