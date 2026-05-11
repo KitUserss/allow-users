baa19ccf318a6f5c2680313d56f0e9c00bb27961
KitUserss <naotemsuporte@gmail.com>
2026-05-11 00:34:38 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -2,30 +2,12 @@ return {
     "Breno010L", -- Bazukita
     "RUANRODRIGO121121", -- Sak
     "StaffordMakayla783", -- bazuka
-    "JustWX99s", -- Dabruxis
-    "PowerLegendSpark2018", -- Dev Spawn
-    "skatetutu", -- wzz
-    "olliver2045judoca", -- frost Mod Ult
-    "ziovxoo", -- Xuung Divulgador
-    "isaac_344725", -- Gagadev divulgador
-    "sansu2449", -- mv Divulgador
-    "sadzin131", -- pietro divulgador
-    "sansu78906", -- mv Divulgador
-    "Luckzvxs", -- luckz divulgador
-    "vteteus", -- [Divulgador] Pietro 07/04/26 01/01/29
-    "higorFARM_FM", -- Higzada "possível divulgador"
     "andrielebibi", -- driih 03/03 03/07
     "adm_Zago98", -- diego permanente
     "K1LL_py", -- kill 04/03 04/07
-    "eduardo_billgate", -- 𝔇𝔲𝔪 #F the best 26/04/26 10/09/53
     "Milena_Martins77", -- 𝕮𝖗𝖚𝖊𝖑𝖑𝖆 𝕱𝖆𝖗𝖗𝖆𝖕𝖔 25/04/26 21/03/64
-    "xVenomDev", -- 𝚅𝚎𝚗𝚘𝚖 02/04/26 18/08/53
-    "ddrrfdrrrrrrr", -- absoluto zero lindo 24/04/26 28/06/45
     "testevinivini", -- mrt00 06/05/26 06/06/26
     "simeumsm3", -- ★~𝓛𝓪𝓻𝓲 ~★ 26/04/26 22/03/64
-    "LUIGI_CRIA9987", -- Sky 26/04/26 11/09/53
-    "Soares_rlk65", -- SOARES 27/04/26 08/03/59
-    "KuonNL", -- 𝔇𝔲𝔪 ANGEL 27/04/26 05/03/59
     "sammy_lovedahy" -- muie do ryu
     "lol6x10" -- 10/05 17/05
 }
