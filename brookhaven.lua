5a4bfba50d985e87aea4298be368be1ed29f6db5
KitUserss <naotemsuporte@gmail.com>
2026-05-11 12:27:15 -0300

    Fix syntax error in brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -5,9 +5,10 @@ return {
     "andrielebibi", -- driih 03/03 03/07
     "adm_Zago98", -- diego permanente
     "K1LL_py", -- kill 04/03 04/07
+    "AXSgbriel", -- Gabriel 11/06/26 11/06/26
     "Milena_Martins77", -- 𝕮𝖗𝖚𝖊𝖑𝖑𝖆 𝕱𝖆𝖗𝖗𝖆𝖕𝖔 25/04/26 21/03/64
     "testevinivini", -- mrt00 06/05/26 06/06/26
     "simeumsm3", -- ★~𝓛𝓪𝓻𝓲 ~★ 26/04/26 22/03/64
-    "sammy_lovedahy" -- muie do ryu
+    "sammy_lovedahy", -- muie do ryu
     "lol6x10" -- 10/05 17/05
 }
