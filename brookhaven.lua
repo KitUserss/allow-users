bdbe54d00d3f4a3390cab5015f709f1caab10cd5
KitUserss <naotemsuporte@gmail.com>
2026-05-11 12:27:49 -0300

    Fix date format for AXSgbriel entry

--- brookhaven.lua
+++ brookhaven.lua
@@ -5,7 +5,7 @@ return {
     "andrielebibi", -- driih 03/03 03/07
     "adm_Zago98", -- diego permanente
     "K1LL_py", -- kill 04/03 04/07
-    "AXSgbriel", -- Gabriel 11/06/26 11/06/26
+    "AXSgbriel", -- Gabriel 11/05/26 11/06/26
     "Milena_Martins77", -- 𝕮𝖗𝖚𝖊𝖑𝖑𝖆 𝕱𝖆𝖗𝖗𝖆𝖕𝖔 25/04/26 21/03/64
     "testevinivini", -- mrt00 06/05/26 06/06/26
     "simeumsm3", -- ★~𝓛𝓪𝓻𝓲 ~★ 26/04/26 22/03/64
