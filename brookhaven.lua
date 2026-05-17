6a54bc5d3dd7a274680be71c9b80c8c4a02aecc5
KitUserss <naotemsuporte@gmail.com>
2026-05-17 15:03:15 -0300

    Update user list in brookhaven.lua
    Removed two usernames and added a new one.

--- brookhaven.lua
+++ brookhaven.lua
@@ -6,11 +6,9 @@ return {
     "adm_Zago98", -- diego permanente
     "K1LL_py", -- kill 04/03 04/07
     "AXSgbriel", -- Gabriel 11/05/26 11/06/26
-    "Milena_Martins77", -- 𝕮𝖗𝖚𝖊𝖑𝖑𝖆 𝕱𝖆𝖗𝖗𝖆𝖕𝖔 25/04/26 21/03/64
     "testevinivini", -- mrt00 06/05/26 06/06/26
     "simeumsm3", -- ★~𝓛𝓪𝓻𝓲 ~★ 26/04/26 22/03/64
     "juniorharker", -- felipe div 26/04/26 22/03/64
     "sammy_lovedahy", -- muie do ryu
-    "lol6x10", -- 10/05 17/05
-    "batataamassada895" -- 14/05 17/05
+    "sem_chat727" - 17/05 22/05
 }
