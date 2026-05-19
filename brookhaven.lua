90e7a85f2e0d88bb2c3fc0cd8c0703c9c688e148
KitUserss <naotemsuporte@gmail.com>
2026-05-18 21:42:57 -0300

    Update comment for Wzleandro7 in brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -13,7 +13,7 @@ return {
     "mikeyzinho49", -- felipe div 26/04/26 22/03/64
     "sammy_lovedahy", -- muie do ryu
     "sem_chat727", -- 17/05 22/05
-    "Wzleandro7", -- Wzleandro7 17/05/26 24/05/26
+    "Wzleandro7", -- Wzleandro7 Criador De Ids
     "dioguebas11", -- 17/05 22/05
     "jog7protrem", -- LN Dev 17/05/26 24/05/26
     "luan04082011" -- LN Dev 17/05/26 24/05/26
