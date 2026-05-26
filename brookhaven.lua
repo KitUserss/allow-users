f08d5594e3a4c6a1def09c4dc1cf7b667d01d11a
KitUserss <naotemsuporte@gmail.com>
2026-05-25 22:18:52 -0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -14,7 +14,6 @@ return {
     "simeumsm3", -- ★~𝓛𝓪𝓻𝓲 ~★ 26/04/26 22/03/64
     "mikeyzinho49", -- felipe div 26/04/26 22/03/64
     "sammy_lovedahy", -- muie do ryu
-    "sem_chat727", -- 17/05 22/05
     "Wzleandro7", -- Wzleandro7 Criador De Ids
     "Conta_desconhecid47", -- ༆TheClockworkDev⛤ 22/05/26 15/02/29
     "Joao12ksksk", -- S a m u e l 22/05/26 15/02/29
@@ -23,6 +22,7 @@ return {
     "VITORXD123C", -- rw 23/05/26 01/08/26
     "sansu78906", -- [Staff] Meliodas 23/05/26 16/02/29
     "sadzin131", -- Pietroxxpz 24/05/26 08/09/51
+    "RYMIFRUIT", -- ZΞryan.exe 25/05/26 04/06/26
     "dodo212110", -- ZEROSEIS 24/05/26 27/05/26
     "roubei_brairot23" -- 24/05 27/05
 }
