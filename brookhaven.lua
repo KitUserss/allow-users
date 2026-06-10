3484e008aa74df6e9f5e96d1b721777aa0fe1e4b
KitUserss <naotemsuporte@gmail.com>
2026-06-10 01:34:15 -0300

    Auto remove expirados: Its_foguinho

--- brookhaven.lua
+++ brookhaven.lua
@@ -26,6 +26,5 @@ return {
     "ibson_queiroz", -- Assaltante com sede e TDAH 04/06/26 11/06/26
     "TrollLinux", -- s 05/06/26 01/03/29
     "Otavio3D10 cliente", -- D4RKsboxx 06/06/26 13/06/26
-    "Two_nov", -- Demon 08/06/26 11/06/26
-    "Its_foguinho" -- The Realy'🥷🏽 07/06/26 10/06/26
+    "Two_nov" -- Demon 08/06/26 11/06/26
 }
