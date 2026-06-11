2fbe24aad77b1f97b406d57e6cb7dcad333029c7
KitUserss <naotemsuporte@gmail.com>
2026-06-11 01:34:15 -0300

    Auto remove expirados: AXSgbriel, 78Rafaa, itz_blackzinh01, ibson_queiroz, Two_nov

--- brookhaven.lua
+++ brookhaven.lua
@@ -7,7 +7,6 @@ return {
     "adm_Zago98", -- diego permanente
     "Eduardo_billgate", -- Filho do ryu
     "K1LL_py", -- kill 04/03 04/07
-    "AXSgbriel", -- Gabriel 11/05/26 11/06/26
     "cantanguandi2", -- 𝐏𝐇𝐙𝐈𝐍 22/05/26 15/02/29
     "simeumsm3", -- ★~𝓛𝓪𝓻𝓲 ~★ 26/04/26 22/03/64
     "mikeyzinho49", -- felipe div 26/04/26 22/03/64
@@ -19,13 +18,9 @@ return {
     "sadzin131", -- Pietroxxpz 24/05/26 08/09/51
     "roubei_brairot23", -- 24/05 27/05
     "Sem_chat727", -- ! Dark 26/05/26 19/02/29
-    "78Rafaa", -- RF 04/06/26 11/06/26
     "p9hasahs", -- VETERANO Palacio ム 26/05/26 12/11/53
     "adminbrookhavenr", -- teste-
-    "itz_blackzinh01", -- 𝑱𝑩 04/06/26 11/06/26
-    "ibson_queiroz", -- Assaltante com sede e TDAH 04/06/26 11/06/26
     "TrollLinux", -- s 05/06/26 01/03/29
     "Otavio3D10 cliente", -- D4RKsboxx 06/06/26 13/06/26
-    "Two_nov", -- Demon 08/06/26 11/06/26
     "eu_nsouyay" -- Bazuka 10/06/26 09/08/26
 }
