2746ecbf2db66ec7ad9921a0da3efd12b45aa99f
KitUserss <naotemsuporte@gmail.com>
2026-06-17 01:34:15 -0300

    Auto remove expirados: Zueiro_bilionario

--- brookhaven.lua
+++ brookhaven.lua
@@ -21,6 +21,5 @@ return {
     "TrollLinux", -- s 05/06/26 01/03/29
     "eu_nsouyay", -- Bazuka 10/06/26 09/08/26
     "two_nov", -- W 11/06/26 27/10/53
-    "Zueiro_bilionario", -- Dark7 14/06/26 17/06/26
     "Strix_Thiago" -- Thiozin 16/06/26 21/06/26
 }
