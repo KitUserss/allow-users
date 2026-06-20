45b887293690480f94ca84123a2c267468c3e6e8
KitUserss <naotemsuporte@gmail.com>
2026-06-20 08:49:43 -0300

    Remove sansu78906

--- brookhaven.lua
+++ brookhaven.lua
@@ -11,7 +11,6 @@ return {
     "Conta_desconhecid47", -- ༆TheClockworkDev⛤ 22/05/26 15/02/29
     "Joao12ksksk", -- S a m u e l 22/05/26 15/02/29
     "VITORXD123C", -- rw 23/05/26 01/08/26
-    "sansu78906", -- [Staff] Meliodas 23/05/26 16/02/29
     "roubei_brairot23", -- 24/05 27/05
     "p9hasahs", -- VETERANO Palacio ム 26/05/26 12/11/53
     "adminbrookhavenr", -- teste-
