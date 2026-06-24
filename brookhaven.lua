fdf3786e41522c860551342f6066b5b521aa2162
KitUserss <naotemsuporte@gmail.com>
2026-06-24 14:40:50 -0300

    Update gd123gg1

--- brookhaven.lua
+++ brookhaven.lua
@@ -14,5 +14,6 @@ return {
     "Sem_chat727", -- 2s 23/06/26 18/03/29
     "l2_tui", -- 𝖕𝖆𝖗𝖆𝖓𝖔𝖎𝖉 23/06/26 08/11/53
     "Sandy_Frank66", -- Biel 24/06/26 24/07/26
-    "Otavio3D10" -- D4RKsboxx 24/06/26 07/07/26
+    "Otavio3D10", -- D4RKsboxx 24/06/26 07/07/26
+    "gd123gg1" -- ☆Black sem pai★ 24/06/26 29/06/26
 }
