d475f0b240c28ab44a36cf0409201cfbc819fd55
KitUserss <naotemsuporte@gmail.com>
2026-06-25 07:28:36 -0300

    Remove Sem_chat727

--- brookhaven.lua
+++ brookhaven.lua
@@ -11,7 +11,6 @@ return {
     "alusonve", -- Victor 19/06/26 19/07/26
     "adminbrookhavenr", -- Victor 24/06/26 09/11/53
     "itz_blackzinh01", -- Max 20/06/26 05/11/53
-    "Sem_chat727", -- 2s 23/06/26 18/03/29
     "l2_tui", -- 𝖕𝖆𝖗𝖆𝖓𝖔𝖎𝖉 23/06/26 08/11/53
     "Sandy_Frank66", -- Biel 24/06/26 24/07/26
     "Otavio3D10", -- D4RKsboxx 24/06/26 07/07/26
