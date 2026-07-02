0344450134138ce8415c5ace109c81e2a8a5f915
KitUserss <naotemsuporte@gmail.com>
2026-07-02 15:35:43 -0300

    Update musclerebirth07

--- brookhaven.lua
+++ brookhaven.lua
@@ -19,5 +19,6 @@ return {
     "Pretizinzn_8", -- P A H 24/06/26 14/07/26
     "gabrielgrande034", -- SANJI 24/06/26 14/07/26
     "user_desconh2", -- Aliás 24/06/26 24/07/26
-    "kzrrzs4r" -- Beca 24/06/26 09/11/53
+    "kzrrzs4r", -- Beca 24/06/26 09/11/53
+    "musclerebirth07" -- Bazuka 02/07/26 05/07/26
 }
