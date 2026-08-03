ee33a102998797403a6ec7f44e97afd7cba9b3f8
KitUserss <Hevseverton@gmail.com>
2026-08-03 04:40:08 -0300

    Update Pedriinn_gamerr

--- brookhaven.lua
+++ brookhaven.lua
@@ -22,6 +22,7 @@ return {
     "Hdjdhdhurhrjtl", -- mmbielzinn_57275 02/08/26 09/08/26
     "its_foguinho", -- ピンクン 02/08/26 06/08/26
     "vitinndelasx3", -- Sixxx 03/08/26 06/08/26
-    "PEEBLE_PRO" -- Isaca 03/08/26 06/08/26
+    "PEEBLE_PRO", -- Isaca 03/08/26 06/08/26
+    "Pedriinn_gamerr" -- lancaster 03/08/26 06/08/26
 }
     
