bd577011f11d383cf1b486c53f6e5a472c86ca1d
KitUserss <Hevseverton@gmail.com>
2026-08-02 21:53:54 -0300

    Auto remove expirados: silvaa_lenda, Toddex019, Otavio3D10

--- brookhaven.lua
+++ brookhaven.lua
@@ -10,9 +10,6 @@ return {
     "kzrrzs4r", -- Beca 24/06/26 09/11/53
     "Sem_chat727", -- jumpscare 19/07/26 19/07/27
     "PODEROSO_PH082", -- THE EMPEROR GEBEH!. 25/07/26 14/08/26
-    "silvaa_lenda", -- silvaxz 26/07/26 02/08/26
-    "Toddex019", -- Toddy 26/07/26 02/08/26
-    "Otavio3D10", -- D4RKsboxx 26/07/26 02/08/26
     "dustsudr", -- icaro 26/07/26 22/06/64
     "ax7_LZINN6", -- yKing 🌠 27/07/26 08/08/26
     "Welix0166", -- ㄥモᜱƗ Տ２ 28/07/26 12/12/53
